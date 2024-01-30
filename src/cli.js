f9f6edf6231d4e138d1100968e5887b0a110cb28
ivann1302 <ivann97n@gmail.com>
2024-01-30 16:14:41 +0300

    исправил замечания линтера

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,10 @@
 import * as input from 'readline-sync';
 
 const greeting = () => {
-	const name = input.question('May I have your name? ');
-	console.log(`Hello, ${name}!`);
+  let name = input.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  const yourName = name;
+  console.log(yourName);
 };
 
 export default greeting;
