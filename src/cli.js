ea1bf0d8073a91e8d2c62059ac3a3e111f3333ac
ivann1302 <ivann97n@gmail.com>
2024-01-30 16:20:29 +0300

    исправил замечания линтера № 2

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import * as input from 'readline-sync';
 
 const greeting = () => {
-  let name = input.question('May I have your name? ');
+  const name = input.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   const yourName = name;
   console.log(yourName);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as readlineSync from 'readline-sync';
+
 let userName;
 let firstRandomNumber;
 let secondRandomNuber;
@@ -11,8 +12,8 @@ let isGameOver;
 
 // Функция приветствия
 const greetings = () => {
-    userName = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
-    console.log(`${'Hello,'} ${userName}${'!'}`);
+  userName = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
+  console.log(`${'Hello,'} ${userName}${'!'}`);
 };
 
 // Функкция определения имени юзера
@@ -20,183 +21,183 @@ const getUsersName = () => userName;
 
 // Функция описания правил игры
 const rulesOfGame = (nameGame) => {
-    switch (nameGame) {
-        case 'brain-game': 
-            console.log('Answer "yes" if the number is even, otherwise answer "no".');
-            break;
-        case 'brain-even' :
-            console.log('Answer "yes" if the number is even, otherwise answer "no".');
-            break;
-        case 'brain-calc' :
-            console.log('What is the result of the expression?');
-            break;
-        case 'brain-gcd' :
-            console.log('Find the greatest common divisor of given numbers.');
-            break;
-        case 'brain-progression' :
-            console.log('What number is missing in the progression?');
-            break;
-        case 'brain-prime' :
-            console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-            break;
-    }
-}
+  switch (nameGame) {
+    case 'brain-game': 
+      console.log('Answer "yes" if the number is even, otherwise answer "no".');
+      break;
+    case 'brain-even':
+      console.log('Answer "yes" if the number is even, otherwise answer "no".');
+      break;
+    case 'brain-calc':
+      console.log('What is the result of the expression?');
+      break;
+    case 'brain-gcd':
+      console.log('Find the greatest common divisor of given numbers.');
+      break;
+    case 'brain-progression':
+      console.log('What number is missing in the progression?');
+      break;
+    case 'brain-prime':
+      console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+      break;
+  }
+};
 
 // Функция вывода рандомного числа
 function getRandom(min, max) {
-    const minCopy = Math.ceil(min);
-    const maxCopy = Math.floor(max);
-    return Math.floor(Math.random() * (maxCopy - minCopy)) + minCopy;
+  const minCopy = Math.ceil(min);
+  const maxCopy = Math.floor(max);
+  return Math.floor(Math.random() * (maxCopy - minCopy)) + minCopy;
 };
 
 // Функция вывода рандомного знака
 const getMathSign = () => {
-    const arr = ['+', '-', '*'];
-    const i = Math.floor(Math.random() * arr.length);
-    const operator = arr[i];
-    return operator;
+  const arr = ['+', '-', '*'];
+  const i = Math.floor(Math.random() * arr.length);
+  const operator = arr[i];
+  return operator;
 };
 
 // Функция вывода прогрессии с загаданным числом
 const getProgression = () => {
-    const num = getRandom(0, 30);
-    const arr = [];
-    stepOfProgression = getRandom(0, 10)
-    let progression = num;
-    hideOfProgression = getRandom(0,10)
-    for (let i = 0; i < 10; i += 1) {
-        arr[i] = progression;
-        progression += stepOfProgression;
-    }
-    hidenNum = arr[hideOfProgression]
-    arr[hideOfProgression] = '..'
-    return arr.join(' ');
+  const num = getRandom(0, 30);
+  const arr = [];
+  stepOfProgression = getRandom(0, 10);
+  let progression = num;
+  hideOfProgression = getRandom(0,10)
+  for (let i = 0; i < 10; i += 1) {
+    arr[i] = progression;
+    progression += stepOfProgression;
+  }
+  hidenNum = arr[hideOfProgression];
+  arr[hideOfProgression] = '..';
+  return arr.join(' ');
 };
 
 // Функция, задающая вопрос юзеру
 const question = (nameGame) => {
-    firstRandomNumber = getRandom(2, 100);
-    secondRandomNuber = getRandom(2, 100);
-    sign = getMathSign();
-    let questionResult;
-    switch (nameGame) {
-        case 'brain-even' :
-            questionResult = console.log(`Question: ${firstRandomNumber}`);
-            break;
-        case 'brain-calc' :
-            questionResult = console.log(`Question: ${firstRandomNumber} ${sign} ${secondRandomNuber}`);
-            break;   
-        case 'brain-gcd' :
-            questionResult = console.log(`Question: ${firstRandomNumber} ${secondRandomNuber}`);
-            break;
-        case 'brain-progression' :
-            questionResult = console.log(`Question: ${getProgression()}`);
-            break;
-        case 'brain-prime' :
-            questionResult = console.log(`Question: ${firstRandomNumber}`);
-            break;
-    }    
+  firstRandomNumber = getRandom(2, 100);
+  secondRandomNuber = getRandom(2, 100);
+  sign = getMathSign();
+  let questionResult;
+  switch (nameGame) {
+    case 'brain-even':
+      questionResult = console.log(`Question: ${firstRandomNumber}`);
+      break;
+    case 'brain-calc':
+      questionResult = console.log(`Question: ${firstRandomNumber} ${sign} ${secondRandomNuber}`);
+      break;   
+    case 'brain-gcd':
+      questionResult = console.log(`Question: ${firstRandomNumber} ${secondRandomNuber}`);
+      break;
+    case 'brain-progression':
+      questionResult = console.log(`Question: ${getProgression()}`);
+      break;
+    case 'brain-prime':
+      questionResult = console.log(`Question: ${firstRandomNumber}`);
+      break;
+  }    
     return questionResult;
-}
+};
 
 // Функция получения ответа от пользователя
 const getUsersAnswer = () => readlineSync.question('Your answer: ');
 
 // Функция расчета правильно ответа для игры brain-even
 const brainEvenCorrectAnswer = (num) => {
-    if (num % 2 === 0) {
-        resultOfCorrectAnswer = 'yes';
-    } else if (num % 2 !== 0) {
-        resultOfCorrectAnswer = 'no';
-    }
-    return resultOfCorrectAnswer;
+  if (num % 2 === 0) {
+    resultOfCorrectAnswer = 'yes';
+  } else if (num % 2 !== 0) {
+    resultOfCorrectAnswer = 'no';
+  }
+  return resultOfCorrectAnswer;
 };
 
 // Функция расчета правильного ответа для игры brain-calc
 const brainCalcCorrectAnswer = (num1, num2) => {
-    if (sign === '+') {
-        resultOfCorrectAnswer = num1 + num2;
-    } else if (sign === '-') {
-        resultOfCorrectAnswer = num1 - num2;
-    } else {
-        resultOfCorrectAnswer = num1 * num2;
-    }
-    return resultOfCorrectAnswer;
+  if (sign === '+') {
+    resultOfCorrectAnswer = num1 + num2;
+  } else if (sign === '-') {
+    resultOfCorrectAnswer = num1 - num2;
+  } else {
+    resultOfCorrectAnswer = num1 * num2;
+  }
+   return resultOfCorrectAnswer;
 };
 
 // Функция расчета правильного ответа для игры brain-gcd
 const brainGcdCorrectAnswer = (num1, num2) => {
-    const min = Math.min(num1, num2)
-    for (let i = 0; i <= min; i += 1) {
-        if (num1 % i === 0 && num2 % i === 0) {
-            resultOfCorrectAnswer = i;
-        }
+  const min = Math.min(num1, num2)
+  for (let i = 0; i <= min; i += 1) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      resultOfCorrectAnswer = i;
     }
-    return resultOfCorrectAnswer;
+  }
+  return resultOfCorrectAnswer;
 };
 
 // Функция проверяет является ли число простым
 const brainPrimeCorrectAnswer = (num) => {
-    for (let i = 2; i < num; i += 1) {
-        if (num % i === 0) {
-            return 'no';
-        }
-    } return 'yes';
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return 'no';
+    }
+  } return 'yes';
 };
 
 // Функция определения правильного ответа в зависимости от названия игры
 const correctAnswer = (nameGame) => {
-    switch (nameGame) {
-        case 'brain-even' :
-            brainEvenCorrectAnswer(firstRandomNumber);
-            break;
-        case 'brain-calc' :
-            brainCalcCorrectAnswer(firstRandomNumber, secondRandomNuber);
-            break;
-        case 'brain-gcd' :
-            brainGcdCorrectAnswer(firstRandomNumber, secondRandomNuber);
-            break;
-        case 'brain-progression' :
-            resultOfCorrectAnswer = hidenNum;
-            break;
-        case 'brain-prime' :
-            resultOfCorrectAnswer = brainPrimeCorrectAnswer(firstRandomNumber);
-            break;
-    } return resultOfCorrectAnswer.toString();
+  switch (nameGame) {
+    case 'brain-even':
+      brainEvenCorrectAnswer(firstRandomNumber);
+      break;
+    case 'brain-calc':
+      brainCalcCorrectAnswer(firstRandomNumber, secondRandomNuber);
+      break;
+    case 'brain-gcd':
+      brainGcdCorrectAnswer(firstRandomNumber, secondRandomNuber);
+      break;
+    case 'brain-progression':
+      resultOfCorrectAnswer = hidenNum;
+      break;
+    case 'brain-prime':
+      resultOfCorrectAnswer = brainPrimeCorrectAnswer(firstRandomNumber);
+      break;
+  } return resultOfCorrectAnswer.toString();
 };
 
 // Функция с выводом текста правильного ответа
 const textOfcorrectAnswer = () => {
     console.log('Correct!');
-}
+};
 
   // Функция сравнения правильного результата с результатом юзера
 const compareOfAnswer = (nameGame) => {
-    const userAnswer = getUsersAnswer();
-    const answer = correctAnswer(nameGame);
-    if (answer === userAnswer) {
-        textOfcorrectAnswer();
-    } else {
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was: '${answer}'.\n${"Let's try again,"} ${getUsersName()}!`);
-        isGameOver = 'true';
-    }
+  const userAnswer = getUsersAnswer();
+  const answer = correctAnswer(nameGame);
+  if (answer === userAnswer) {
+    textOfcorrectAnswer();
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was: '${answer}'.\n${"Let's try again,"} ${getUsersName()}!`);
+    isGameOver = 'true';
+  }
 };
 
 // Функция запуска игры с количеством раундов равным 3
 const runGameWithCounter = (nameGame) => {
-    greetings();
-    rulesOfGame(nameGame);
-    const count = 3;
-    let i = 0;
-    while (i < count && isGameOver !== 'true') {
-        question(nameGame);
-        correctAnswer(nameGame);
-        compareOfAnswer(nameGame);
-        i += 1;
-    }
-    if (i === 3 & isGameOver !== 'true') {
-        console.log(`Congratulations, ${getUsersName()}!`);
-    }
+  greetings();
+  rulesOfGame(nameGame);
+  const count = 3;
+  let i = 0;
+  while (i < count && isGameOver !== 'true') {
+    question(nameGame);
+    correctAnswer(nameGame);
+    compareOfAnswer(nameGame);
+    i += 1;
+  }
+  if (i === 3 && isGameOver !== 'true') {
+    console.log(`Congratulations, ${getUsersName()}!`);
+  }
 };
 
 export default runGameWithCounter;
